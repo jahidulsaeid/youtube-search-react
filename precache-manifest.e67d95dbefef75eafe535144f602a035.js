ab37848b7d14461dfcf001ac6737f7b6e8804ddf
jahidulsaeid <jahdiul.saeid@gmail.com>
2020-08-12 22:51:04 +0600

    Updates

---
+++ precache-manifest.e67d95dbefef75eafe535144f602a035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbd4e3983c1e2774f39f4e52cfc9786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23509120d96421a94df",
+    "url": "/static/js/2.de46505f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de46505f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd837de22e546bb9e75b",
+    "url": "/static/js/main.c81f4a43.chunk.js"
+  },
+  {
+    "revision": "2b891a9cec74f13ac7a4",
+    "url": "/static/js/runtime-main.f0114904.js"
+  }
+]);
